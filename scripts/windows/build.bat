5757ec4325671420e8e0c9db19605552d4402420
Neil Blakey-Milner <nbm@nxsy.org>
2015-01-20 23:00:21 -0800

    Improve vim build/quickfix integration with full paths in vim

--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -5,6 +5,6 @@ set BUILDDIR="%~dp0\..\..\build"
 IF NOT EXIST %BUILDDIR% mkdir %BUILDDIR%
 pushd %BUILDDIR%
 
-cl /nologo /Zi ..\source\hajonta\platform\win32.cpp /link /incremental:no User32.lib
+cl /FC /nologo /Zi ..\source\hajonta\platform\win32.cpp /link /incremental:no User32.lib
 
 popd
