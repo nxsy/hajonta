2ea4b61bed20e1e36d093ad8e647b5726b04b56c
Neil Blakey-Milner <nbm@nxsy.org>
2015-01-20 23:06:20 -0800

    Minimum OpenGL context creation with wglCreateContext

--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -5,6 +5,6 @@ set BUILDDIR="%~dp0\..\..\build"
 IF NOT EXIST %BUILDDIR% mkdir %BUILDDIR%
 pushd %BUILDDIR%
 
-cl /FC /nologo /Zi ..\source\hajonta\platform\win32.cpp /link /incremental:no User32.lib
+cl /FC /nologo /Zi ..\source\hajonta\platform\win32.cpp /link /incremental:no User32.lib Gdi32.lib Opengl32.lib
 
 popd
--- source/hajonta/platform/win32.cpp
+++ source/hajonta/platform/win32.cpp
@@ -2,10 +2,13 @@
 #include <stdio.h>
 
 #include <windows.h>
+#include <gl/gl.h>
 
 struct platform_state
 {
     int32_t stopping;
+
+    HDC device_context;
 };
 
 LRESULT CALLBACK
@@ -16,13 +19,13 @@ main_window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
         state = (platform_state *)(((LPCREATESTRUCT)lParam)->lpCreateParams);
         SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)state);
-        OutputDebugStringA("WM_NCCREATE called\n");
     }
     else
     {
         state = (platform_state *)GetWindowLongPtr(hwnd, GWLP_USERDATA);
         if (!state)
         {
+            // This message is allowed to arrive before WM_NCCREATE (!)
             if (uMsg != WM_GETMINMAXINFO)
             {
                 char debug_string[1024] = {};
@@ -37,6 +40,62 @@ main_window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
     switch (uMsg)
     {
+        case WM_CREATE:
+        {
+            PIXELFORMATDESCRIPTOR pixel_format_descriptor = {
+                sizeof(PIXELFORMATDESCRIPTOR),
+                1,
+                PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+                PFD_TYPE_RGBA,
+                32,
+                0, 0, 0, 0, 0, 0,
+                0,
+                0,
+                0,
+                0, 0, 0, 0,
+                24,
+                8,
+                0,
+                PFD_MAIN_PLANE,
+                0,
+                0, 0, 0
+            };
+
+            state->device_context = GetDC(hwnd);
+            if (!state->device_context)
+            {
+                MessageBoxA(0, "Unable to acquire device context", 0, MB_OK | MB_ICONSTOP);
+                return 1;
+            }
+
+            int pixel_format = ChoosePixelFormat(state->device_context, &pixel_format_descriptor);
+            if (!pixel_format)
+            {
+                MessageBoxA(0, "Unable to choose requested pixel format", 0, MB_OK | MB_ICONSTOP);
+                return 1;
+            }
+
+            if(!SetPixelFormat(state->device_context, pixel_format, &pixel_format_descriptor))
+            {
+                MessageBoxA(0, "Unable to set pixel format", 0, MB_OK | MB_ICONSTOP);
+                return 1;
+            }
+
+            HGLRC wgl_context = wglCreateContext(state->device_context);
+            if(!wgl_context)
+            {
+                MessageBoxA(0, "Unable to create wgl context", 0, MB_OK | MB_ICONSTOP);
+                return 1;
+            }
+
+            if(!wglMakeCurrent(state->device_context, wgl_context))
+            {
+                MessageBoxA(0, "Unable to make wgl context current", 0, MB_OK | MB_ICONSTOP);
+                return 1;
+            }
+
+            glViewport(0, 0, 960, 540);
+        } break;
         case WM_CLOSE:
         {
             state->stopping = 1;
@@ -80,21 +139,26 @@ WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdS
     window_class.lpfnWndProc = main_window_callback;
     window_class.hInstance = hInstance;
     window_class.lpszClassName = "NajontaMainWindow";
+    window_class.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
 
     if (!RegisterClassExA(&window_class))
     {
         MessageBoxA(0, "Unable to register window class", 0, MB_OK | MB_ICONSTOP);
         return 1;
     }
+
+    int width = 960;
+    int height = 540;
+
     HWND window = CreateWindowExA(
         0,
         "NajontaMainWindow",
         "Najonta",
-        WS_OVERLAPPEDWINDOW,
+        WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
         0,
         0,
-        960,
-        540,
+        width,
+        height,
         0,
         0,
         hInstance,
@@ -109,9 +173,29 @@ WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdS
 
     ShowWindow(window, nCmdShow);
 
+    float red = .0f, green = .0f, blue = .0f;
+
     while(!state.stopping)
     {
+        red += 0.0001f;
+        green += 0.00001f;
+        blue += 0.000001f;
+        if (red > 1.0f)
+        {
+            red -= 1;
+        }
+        if (green > 1.0f)
+        {
+            green -= 1;
+        }
+        if (blue > 1.0f)
+        {
+            blue -= 1;
+        }
         handle_win32_messages(&state);
+        glClearColor(red, green, blue, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        SwapBuffers(state.device_context);
     }
 
     return 0;
