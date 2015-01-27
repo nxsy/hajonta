2c619ba62cbcfeb98233d994df059a2eea657832
Neil Blakey-Milner <nbm@nxsy.org>
2015-01-26 21:37:25 -0800

    Update program bootstrap to support Linux

--- source/hajonta/bootstrap/program.cpp
+++ source/hajonta/bootstrap/program.cpp
@@ -1,18 +1,32 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/stat.h>
 
+#if defined(_WIN32)
 #include <windows.h>
+#define SLASH "\\"
+#elif defined(__linux__)
+#include <linux/limits.h>
+#define MAX_PATH PATH_MAX
+#define _snprintf snprintf
+#define SLASH "/"
+#endif
 
 int
 mkdir_recursively(char *path)
 {
     int path_offset = 0;
-    while(char *next_slash = strstr(path + path_offset, "\\"))
+    while(char *next_slash = strstr(path + path_offset, SLASH))
     {
         path_offset = next_slash - path + 1;
         char directory[MAX_PATH];
-        _snprintf(directory, MAX_PATH, "%.*s", next_slash - path, path);
+        _snprintf(directory, MAX_PATH, "%.*s", (int32_t)(next_slash - path), path);
+#if defined(_WIN32)
         CreateDirectory(directory, 0);
+#else
+        mkdir(directory, 0777);
+#endif
     }
     return 0;
 }
@@ -30,9 +44,9 @@ main(int argc, char **argv)
     }
 
     char vertexshader[MAX_PATH] = {};
-    _snprintf(vertexshader, sizeof(vertexshader), "%s\\%s\\%s\\vertex.glsl", argv[1], argv[2], argv[3]);
+    _snprintf(vertexshader, sizeof(vertexshader), "%s%s%s%s%s%svertex.glsl", argv[1], SLASH, argv[2], SLASH, argv[3], SLASH);
     char fragshader[MAX_PATH] = {};
-    _snprintf(fragshader, sizeof(fragshader), "%s\\%s\\%s\\fragment.glsl", argv[1], argv[2], argv[3]);
+    _snprintf(fragshader, sizeof(fragshader), "%s%s%s%s%s%sfragment.glsl", argv[1], SLASH, argv[2], SLASH, argv[3], SLASH);
 
     FILE *v = fopen(vertexshader, "r");
     FILE *f = fopen(fragshader, "r");
@@ -49,7 +63,7 @@ main(int argc, char **argv)
     }
 
     char outputprogram[MAX_PATH] = {};
-    _snprintf(outputprogram, sizeof(outputprogram), "generated\\%s\\%s.h", argv[2], argv[3]);
+    _snprintf(outputprogram, sizeof(outputprogram), "generated%s%s%s%s.h", SLASH, argv[2], SLASH, argv[3]);
     mkdir_recursively(outputprogram);
     char outputprogramtemp[MAX_PATH] = {};
     _snprintf(outputprogramtemp, sizeof(outputprogramtemp), "%s.tmp", outputprogram);
@@ -109,7 +123,7 @@ main(int argc, char **argv)
     {
         uint32_t shader = vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
         int compiled;
-        glShaderSource(shader, 1, &vertex_shader_source, 0);
+        glShaderSource(shader, 1, (const char **)&vertex_shader_source, 0);
         glCompileShader(shader);
         glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
         if (!compiled)
@@ -123,7 +137,7 @@ main(int argc, char **argv)
     {
         uint32_t shader = fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
         int compiled;
-        glShaderSource(shader, 1, &fragment_shader_source, 0);
+        glShaderSource(shader, 1, (const char**)&fragment_shader_source, 0);
         glCompileShader(shader);
         glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
         if (!compiled)
@@ -142,7 +156,7 @@ main(int argc, char **argv)
     glGetProgramiv(state->program, GL_LINK_STATUS, &linked);
     if (!linked)
     {
-        char info_log[1024];
+        char info_log[1024] = {};
         glGetProgramInfoLog(state->program, sizeof(info_log), 0, info_log);
         memory->platform_fail(ctx, info_log);
         return false;
@@ -158,5 +172,22 @@ main(int argc, char **argv)
     fwrite(buffer, 1, strlen(buffer), p);
     fclose(p);
 
+#if defined(_WIN32)
     CopyFile(outputprogramtemp, outputprogram, 0);
+#else
+    FILE *temp = fopen(outputprogramtemp, "r");
+    FILE *final = fopen(outputprogram, "w");
+
+    while (feof(temp) == 0)
+    {
+        int size_read = fread(buffer, 1, sizeof(buffer), temp);
+        int size_write = fwrite(buffer, 1, size_read, final);
+        if (size_read != size_write)
+        {
+            printf("size_read (%d) != size_write(%d)\n", size_read, size_write);
+            return 1;
+        }
+    }
+    fclose(f);
+#endif
 }
