3b5e7d4ed3f5d136e8f95350c64fa57a1a3dbae9
Neil Blakey-Milner <nbm@nxsy.org>
2015-02-17 14:24:16 -0800

    Only try run unit tests after we've built the unit test binary!

--- scripts/xcb/build.sh
+++ scripts/xcb/build.sh
@@ -16,14 +16,13 @@ cd ${BASEDIR}
 mkdir -p build
 mkdir -p build/debug
 
-(cd build/debug && ./unit )
-
 ${CC} ${CPPFLAGS} -std=c++0x ${WARNFLAGS} -o build/debug/program source/hajonta/bootstrap/program.cpp ${CPPFLAGS} ${DEBUG_FLAGS}
 (cd build/debug && ./program ../../source hajonta/programs a )
 (cd build/debug && ./program ../../source hajonta/programs debug_font )
 (cd build/debug && ./program ../../source hajonta/programs b )
 
 ${CC} ${CPPFLAGS} ${INCLUDES} ${WARNFLAGS} -o build/debug/unit source/hajonta/bootstrap/unit.cpp ${CPPFLAGS} ${DEBUG_FLAGS} -lm
+(cd build/debug && ./unit )
 
 ${CC} ${CPPFLAGS} -Isource -Ibuild/debug/generated -std=c++11 ${WARNFLAGS} -shared -Wl,-soname,libgame.so.1 -fPIC -o build/debug/libgame.so.new source/hajonta/game.cpp ${CPPFLAGS} ${DEBUG_FLAGS}
 mv -f build/debug/libgame.so.new build/debug/libgame.so
