6ca9308a8c0732f869e3536f84093c01cdc97496
Neil Blakey-Milner <nbm@nxsy.org>
2015-02-20 18:25:38 -0800

    Compile with Visual Studio, and with no warnings.

--- source/hajonta/image.cpp
+++ source/hajonta/image.cpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #define STB_IMAGE_IMPLEMENTATION
+
+#pragma warning(push, 4)
+#pragma warning(disable: 4365 4312)
 #include "hajonta/thirdparty/stb_image.h"
+#pragma warning(pop)
 
 bool
 load_image(uint8_t *source, uint32_t source_size, uint8_t *dest, uint32_t dest_size)
@@ -9,14 +13,14 @@ load_image(uint8_t *source, uint32_t source_size, uint8_t *dest, uint32_t dest_s
     int32_t x;
     int32_t y;
     int32_t comp;
-    unsigned char *result = stbi_load_from_memory((unsigned char *)source, source_size, &x, &y, &comp, STBI_rgb_alpha);
+    unsigned char *result = stbi_load_from_memory((unsigned char *)source, (int32_t)source_size, &x, &y, &comp, STBI_rgb_alpha);
 
     if (result == 0)
     {
         return false;
     }
 
-    uint32_t actual_size = x * y * 4;
+    uint32_t actual_size = x * y * (uint32_t)4;
     if (actual_size != dest_size)
     {
         return false;
