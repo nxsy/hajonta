e52d620879c9467de4fb33c3e2090af42cf9cca0
Neil Blakey-Milner <nbm@nxsy.org>
2015-06-20 14:54:06 -0700

    [firework] basic firework motion physics

--- source/hajonta/demos/firework.cpp
+++ source/hajonta/demos/firework.cpp
@@ -15,16 +15,6 @@
 #include "hajonta/programs/ui2d.h"
 #include "hajonta/ui/ui2d.cpp"
 
-struct firework_particle
-{
-    v3 position;
-    v3 velocity;
-
-    v3 constant_acceleration;
-
-    v3 force_accumulator;
-};
-
 enum struct shader_mode
 {
     standard,
@@ -98,7 +88,7 @@ DEMO(demo_firework)
             { 9,10, 5},
             {10, 6, 1},
         };
-        demo_state->num_faces = harray_count(faces);
+        demo_state->firework_num_faces = harray_count(faces);
         glGenBuffers(1, &demo_state->firework_ibo);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, demo_state->firework_ibo);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER,
@@ -113,6 +103,77 @@ DEMO(demo_firework)
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, &color);
 
         glErrorAssert();
+
+        {
+            glGenBuffers(1, &demo_state->ground_vbo);
+            struct
+            {
+                float position[4];
+            } vertices[] = {
+                {-200.000f, 0.000f,-100.000f, 1.000f},
+                {-200.000f, 0.000f, 100.000f, 1.000f},
+                { 200.000f, 0.000f,-100.000f, 1.000f},
+                { 200.000f, 0.000f, 100.000f, 1.000f},
+            };
+            glBindBuffer(GL_ARRAY_BUFFER, demo_state->ground_vbo);
+            glBufferData(GL_ARRAY_BUFFER,
+                    sizeof(vertices),
+                    vertices,
+                    GL_STATIC_DRAW);
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+            struct
+            {
+                uint8_t vertex_ids[3];
+            } faces[] =
+            {
+                { 0, 1, 2},
+                { 0, 2, 3},
+            };
+            demo_state->ground_num_faces = harray_count(faces);
+            glGenBuffers(1, &demo_state->ground_ibo);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, demo_state->ground_ibo);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                    sizeof(faces),
+                    faces,
+                    GL_STATIC_DRAW);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+            glGenTextures(1, &demo_state->ground_texture);
+            glBindTexture(GL_TEXTURE_2D, demo_state->ground_texture);
+            uint32_t color = 0xff202020;
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, &color);
+
+            glErrorAssert();
+        }
+
+        firework_particle particles[] =
+        {
+            {
+                { 0.0f, 10.0f,-26.0f},
+                { 0.0f, 15.0f, -5.0f},
+                { 0.0f, -9.8f,  0.0f},
+                { 0.0f,  0.0f,  0.0f},
+            },
+            {
+                { 5.0f, 20.0f,-38.0f},
+                { 3.0f, 10.0f,  0.0f},
+                { 0.0f, -9.8f,  0.0f},
+                { 0.0f,  0.0f,  0.0f},
+            },
+            {
+                {-5.0f,  5.0f,-50.0f},
+                {10.0f, 20.0f,  7.0f},
+                { 0.0f, -9.8f,  0.0f},
+                { 0.0f,  0.0f,  0.0f},
+            },
+        };
+        uint32_t num_particles = harray_count(particles);
+        for (uint32_t idx = 0; idx < num_particles; ++idx)
+        {
+            demo_state->particles[idx] = particles[idx];
+        }
+        demo_state->num_particles = num_particles;
     }
 
     for (uint32_t i = 0;
@@ -159,6 +220,7 @@ DEMO(demo_firework)
     glErrorAssert();
 
     m4 view = m4identity();
+    view.cols[3] = v4{0,-6,0,1};
     float ratio = (float)input->window.width / (float)input->window.height;
     m4 projection = m4frustumprojection(1.0f, 100.0f, {-ratio, -1.0f}, {ratio, 1.0f});
 
@@ -167,40 +229,57 @@ DEMO(demo_firework)
 
     glErrorAssert();
 
-    firework_particle particles[] =
+    for (uint32_t idx = 0; idx < demo_state->num_particles; ++idx)
     {
-        {
-            {0, 0, -6.0f},
-            {0, 0, 0},
-            {0, 0, 0},
-            {0, 0, 0},
-        },
-        {
-            {1, 1, -8.0f},
-            {0, 0, 0},
-            {0, 0, 0},
-            {0, 0, 0},
-        },
-        {
-            {-1, -1, -10.0f},
-            {0, 0, 0},
-            {0, 0, 0},
-            {0, 0, 0},
-        },
-    };
+        firework_particle *fp = demo_state->particles + idx;
 
-    uint32_t num_particles = harray_count(particles);
+        v3 acceleration = fp->constant_acceleration;
+        acceleration = v3sub(acceleration, v3mul(fp->velocity, 0.05f));
+        v3 movement = v3add(
+            v3mul(acceleration, 0.5f * (input->delta_t * input->delta_t)),
+            v3mul(fp->velocity, input->delta_t)
+        );
+        fp->position = v3add(fp->position, movement);
+        fp->velocity = v3add(
+            v3mul(acceleration, input->delta_t),
+            fp->velocity
+        );
 
-    for (uint32_t idx = 0; idx < num_particles; ++idx)
-    {
+        if (fp->position.y < 0.0f)
+        {
+            firework_particle *last_particle = demo_state->particles + demo_state->num_particles - 1;
+            *fp = *last_particle;
+            demo_state->num_particles--;
+            idx--;
+            continue;
+        }
+
         m4 model = m4identity();
-        firework_particle *fp = particles + idx;
         m4 translate = m4identity();
         translate.cols[3] = v4{fp->position.x, fp->position.y, fp->position.z, 1.000f};
         model = m4mul(model, translate);
 
         glUniformMatrix4fv(state->program_c.u_model_id, 1, false, (float *)&model);
-        glDrawElements(GL_TRIANGLES, (GLsizei)(demo_state->num_faces * 3), GL_UNSIGNED_BYTE, 0);
+        glDrawElements(GL_TRIANGLES, (GLsizei)(demo_state->firework_num_faces * 3), GL_UNSIGNED_BYTE, 0);
+    }
+
+    glErrorAssert();
+
+    glBindBuffer(GL_ARRAY_BUFFER, demo_state->ground_vbo);
+    glEnableVertexAttribArray((GLuint)state->program_c.a_pos_id);
+    glVertexAttribPointer((GLuint)state->program_c.a_pos_id, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, demo_state->ground_ibo);
+
+    glUniform1i(
+        glGetUniformLocation(state->program_c.program, "tex"),
+        0);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, demo_state->ground_texture);
+
+    {
+        m4 model = m4identity();
+        glUniformMatrix4fv(state->program_c.u_model_id, 1, false, (float *)&model);
+        glDrawElements(GL_TRIANGLES, (GLsizei)(demo_state->firework_num_faces * 3), GL_UNSIGNED_BYTE, 0);
     }
 
     glErrorAssert();
@@ -210,4 +289,9 @@ DEMO(demo_firework)
     glBindTexture(GL_TEXTURE_2D, 0);
 
     glErrorAssert();
+
+    if (!demo_state->num_particles)
+    {
+        memory->quit = true;
+    }
 }
--- source/hajonta/demos/firework.h
+++ source/hajonta/demos/firework.h
@@ -1,13 +1,30 @@
 #pragma once
 
+struct firework_particle
+{
+    v3 position;
+    v3 velocity;
+
+    v3 constant_acceleration;
+
+    v3 force_accumulator;
+};
+
 struct demo_firework_state {
     float delta_t;
 
     uint32_t firework_vbo;
     uint32_t firework_ibo;
     uint32_t firework_texture;
+    uint32_t firework_num_faces;
 
-    uint32_t num_faces;
+    uint32_t ground_vbo;
+    uint32_t ground_ibo;
+    uint32_t ground_texture;
+    uint32_t ground_num_faces;
+
+    uint32_t num_particles;
+    firework_particle particles[1024];
 };
 
 DEMO(demo_firework);
