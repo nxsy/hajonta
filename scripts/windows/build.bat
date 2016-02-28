f96d7b7ad7f28ef823ba53274bacc564d4f09de9
Neil Blakey-Milner <nbm@nxsy.org>
2016-02-27 18:32:03 -0800

    [windows] some build/env changes

--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -10,17 +10,17 @@ set INCLUDES=-I..\source -Igenerated -Zi -I..\source\hajonta\thirdparty -I..\thi
 set CPPFLAGS=%includes% /FC /nologo /Wall /wd4820 /wd4668 /wd4996 /wd4100 /wd4514 /wd4191 /wd4201 /wd4505 /wd4710
 
 cl %CPPFLAGS% /Zi ..\source\hajonta\bootstrap\program.cpp /link /incremental:no User32.lib /SUBSYSTEM:CONSOLE
-.\program.exe ..\source hajonta\programs a
-.\program.exe ..\source hajonta\programs debug_font
-.\program.exe ..\source hajonta\programs b
+REM .\program.exe ..\source hajonta\programs a
+REM .\program.exe ..\source hajonta\programs debug_font
+REM .\program.exe ..\source hajonta\programs b
 .\program.exe ..\source hajonta\programs ui2d
-.\program.exe ..\source hajonta\programs skybox
-.\program.exe ..\source hajonta\programs c
+REM .\program.exe ..\source hajonta\programs skybox
+REM .\program.exe ..\source hajonta\programs c
 .\program.exe ..\source hajonta\programs imgui
 cl %CPPFLAGS% /Zi ..\source\hajonta\bootstrap\unit.cpp /link /incremental:no User32.lib /SUBSYSTEM:CONSOLE
 .\unit.exe
 del *.pdb > NUL 2> NUL
-cl %CPPFLAGS% /Zi ..\source\hajonta\bootstrap\win32_msvc.cpp /link /incremental:no -PDB:win32_msvc.pdb User32.lib
+REM cl %CPPFLAGS% /Zi ..\source\hajonta\bootstrap\win32_msvc.cpp /link /incremental:no -PDB:win32_msvc.pdb User32.lib
 
 echo > game2.dll.lock
 cl %CPPFLAGS% -DHAJONTA_DEBUG=1 /Zi ..\source\hajonta\game2.cpp -LD /link /incremental:no -PDB:game-%random%.pdb -EXPORT:game_update_and_render Opengl32.lib
@@ -30,18 +30,18 @@ cl %CPPFLAGS% -DHAJONTA_DEBUG=1 /Zi ..\source\hajonta\renderer\opengl.cpp -LD /l
 del renderer.dll.lock
 cl %CPPFLAGS% -DHAJONTA_LIBRARY_NAME=game2.dll -DHAJONTA_RENDERER_LIBRARY_NAME=opengl.dll -DHAJONTA_DEBUG=1 ..\source\hajonta\platform\win32.cpp /link /incremental:no User32.lib Gdi32.lib Opengl32.lib Xaudio2.lib Ole32.lib Shlwapi.lib
 
-echo > editor.dll.lock
-cl %CPPFLAGS% -DHAJONTA_DEBUG=1 /Zi ..\source\hajonta\utils\editor.cpp -LD /link /incremental:no -PDB:editor-%random%.pdb -EXPORT:game_update_and_render Opengl32.lib
-del editor.dll.lock
-copy ..\source\hajonta\platform\win32.cpp generated\win32_editor.cpp
-cl %CPPFLAGS% -DHAJONTA_LIBRARY_NAME=editor.dll -DHAJONTA_DEBUG=1 generated\win32_editor.cpp /link /incremental:no User32.lib Gdi32.lib Opengl32.lib Xaudio2.lib Ole32.lib Shlwapi.lib
+REM echo > editor.dll.lock
+REM cl %CPPFLAGS% -DHAJONTA_DEBUG=1 /Zi ..\source\hajonta\utils\editor.cpp -LD /link /incremental:no -PDB:editor-%random%.pdb -EXPORT:game_update_and_render Opengl32.lib
+REM del editor.dll.lock
+REM copy ..\source\hajonta\platform\win32.cpp generated\win32_editor.cpp
+REM cl %CPPFLAGS% -DHAJONTA_LIBRARY_NAME=editor.dll -DHAJONTA_DEBUG=1 generated\win32_editor.cpp /link /incremental:no User32.lib Gdi32.lib Opengl32.lib Xaudio2.lib Ole32.lib Shlwapi.lib
 
-echo > ddsviewer.dll.lock
-cl %CPPFLAGS% -DHAJONTA_DEBUG=1 /Zi ..\source\hajonta\utils\ddsviewer.cpp -LD /link /incremental:no -PDB:ddsviewer-%random%.pdb -EXPORT:game_update_and_render Opengl32.lib
-del ddsviewer.dll.lock
-copy ..\source\hajonta\platform\win32.cpp generated\win32_ddsviewer.cpp
-cl %CPPFLAGS% -DHAJONTA_LIBRARY_NAME=ddsviewer.dll -DHAJONTA_DEBUG=1 generated\win32_ddsviewer.cpp /link /incremental:no User32.lib Gdi32.lib Opengl32.lib Xaudio2.lib Ole32.lib Shlwapi.lib
+REM echo > ddsviewer.dll.lock
+REM cl %CPPFLAGS% -DHAJONTA_DEBUG=1 /Zi ..\source\hajonta\utils\ddsviewer.cpp -LD /link /incremental:no -PDB:ddsviewer-%random%.pdb -EXPORT:game_update_and_render Opengl32.lib
+REM del ddsviewer.dll.lock
+REM copy ..\source\hajonta\platform\win32.cpp generated\win32_ddsviewer.cpp
+REM cl %CPPFLAGS% -DHAJONTA_LIBRARY_NAME=ddsviewer.dll -DHAJONTA_DEBUG=1 generated\win32_ddsviewer.cpp /link /incremental:no User32.lib Gdi32.lib Opengl32.lib Xaudio2.lib Ole32.lib Shlwapi.lib
 
-cl %CPPFLAGS% -DHAJONTA_DEBUG=1 /Zi ..\source\hajonta\utils\bump_to_normal.cpp /link /incremental:no User32.lib Gdi32.lib Opengl32.lib Xaudio2.lib Ole32.lib Shlwapi.lib
+REM cl %CPPFLAGS% -DHAJONTA_DEBUG=1 /Zi ..\source\hajonta\utils\bump_to_normal.cpp /link /incremental:no User32.lib Gdi32.lib Opengl32.lib Xaudio2.lib Ole32.lib Shlwapi.lib
 
 popd
--- scripts/windows/shell.bat
+++ scripts/windows/shell.bat
@@ -4,4 +4,4 @@ set VCDIR="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"
 
 call %VCDIR%\vcvarsall.bat x64
 
-set PATH=%~dp0;%PATH%
+set PATH=%~dp0;%PATH%;c:\msys64\usr\bin
