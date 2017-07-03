e9b41235b88ff202f10d1cf9285c28869337570a
Neil Blakey-Milner <nbm@nxsy.org>
2017-07-03 13:29:18 -0700

    Clean out no-longer-used/building code

--- scripts/sdl2/build.bat
+++ scripts/sdl2/build.bat
@@ -3,14 +3,11 @@
 call build_base.bat
 
 cl %CPPFLAGS% /Zi ..\source\hajonta\bootstrap\program.cpp /link /incremental:no User32.lib /SUBSYSTEM:CONSOLE
-.\program.exe ..\source hajonta\programs ui2d
 .\program.exe ..\source hajonta\programs imgui
 .\program.exe ..\source hajonta\programs sky
-.\program.exe ..\source hajonta\programs phong_no_normal_map
 .\program.exe ..\source hajonta\programs texarray_1
 .\program.exe ..\source hajonta\programs texarray_1_vsm
 .\program.exe ..\source hajonta\programs texarray_1_water
-.\program.exe ..\source hajonta\programs variance_shadow_map
 .\program.exe ..\source hajonta\programs\filters filter_gaussian_7x1
 cl %CPPFLAGS% /Zi ..\source\hajonta\bootstrap\unit.cpp /link /incremental:no User32.lib /SUBSYSTEM:CONSOLE
 .\unit.exe
--- source/hajonta/programs/skybox/fragment.eglsl
+++
@@ -1,6 +0,0 @@
-varying vec4 v_color;
-
-void main(void)
-{
-    gl_FragColor = v_color;
-}
