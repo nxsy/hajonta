1058790529e2b9fd23d8ef3470a6683100a73d0f
Neil Blakey-Milner <nbm@nxsy.org>
2017-08-11 19:19:09 -0700

    Visual Studio 2017 migration

--- scripts/sdl2/build_base.bat
+++ scripts/sdl2/build_base.bat
@@ -10,7 +10,8 @@ pushd %BUILDDIR%
 
 set INCLUDES=-I..\source -Igenerated -Zi -I..\source\hajonta\thirdparty -I..\thirdparty\sdl2\include -I..\thirdparty\stb -I..\thirdparty\imgui -I..\thirdparty\tinyobjloader -I..\thirdparty\par
 
-set GAME_WARNINGS=/Wall /wd4820 /wd4668 /wd4996 /wd4100 /wd4514 /wd4191 /wd4201 /wd4505 /wd4710 /wd4625 /wd5026 /wd4626 /wd5027 /wd4711 /wd4429
+set GAME_WARNINGS=/W4 /wd4996 /wd4201 /wd4100
+REM all /wd4820 /wd4668 /wd4996 /wd4100 /wd4514 /wd4191 /wd4201 /wd4505 /wd4710 /wd4625 /wd5026 /wd4626 /wd5027 /wd4711 /wd4429
 set TOOL_WARNINGS=/W4 -D_CRT_SECURE_NO_WARNINGS=1 /wd4201
 set COMMON_CPPFLAGS=%includes% /FC /nologo /Od
 set CPPFLAGS=%COMMON_CPPFLAGS% %GAME_WARNINGS% /EHsc
--- scripts/sdl2/shell.bat
+++ scripts/sdl2/shell.bat
@@ -1,7 +1,8 @@
 @echo off
 
-set VCDIR="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"
-
-call %VCDIR%\vcvarsall.bat x64
+REM set VCDIR="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"
+REM call %VCDIR%\vcvarsall.bat x64
+REM
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools"\VsDevCmd.bat -arch=amd64 -host_arch=amd64
 
 set PATH=%~dp0;%PATH%
